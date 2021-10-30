0b096945b0e45a3fb7e6e03c7a48ef14b5952e30
MonarchMonarch <stogov.maxim@gmail.com>
2021-10-30 13:23:30 +0300

    RISK FAQ

--- src/components/FAQ/Faq.js
+++ src/components/FAQ/Faq.js
@@ -2,7 +2,17 @@ import React from "react"
 
 const Faq = () => {
     return (
-        <div>Hello world</div>
+        <div>
+            <h1>
+                Kas ir RISK?
+            </h1>
+            <p>
+                <b>RISK</b> - mežu ugunsgrēku kopējā riska vērtība. Katram ugunsgrēku ietekmējošam
+                faktoram piešķirta skaitliska vienība (svars), kas raksturo tā ietekmi uz meža
+                ugunsgrēku izcelšanos vai izplatīšanos.
+                
+            </p>
+        </div>
     )
 }
     
