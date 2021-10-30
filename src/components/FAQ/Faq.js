d47c1081992ee3ecc49ed8ce9e73c1333c08ea69
MonarchMonarch <stogov.maxim@gmail.com>
2021-10-30 14:04:20 +0300

    Why every point is red FAQ

--- src/components/FAQ/Faq.js
+++ src/components/FAQ/Faq.js
@@ -8,13 +8,13 @@ const Faq = () => {
                     Kas ir RISK?
                 </h1>
                 <p>
-                    <b>RISK</b> - mežu ugunsgrēku kopējā riska vērtība. Katram ugunsgrēku ietekmējošam
-                    faktoram piešķirta skaitliska vienība (svars), kas raksturo tā ietekmi uz meža
+                    <b>RISK</b> - mežu ugunsgrēku kopējā riska vērtība. Katram ugunsgrēku ietekmējošam 
+                    faktoram piešķirta skaitliska vienība (svars), kas raksturo tā ietekmi uz meža 
                     ugunsgrēku izcelšanos vai izplatīšanos.
                 </p>
                 <p>
-                    <b>RISK</b> - mežu ugunsgrēku kopējā riska vērtība. Katram ugunsgrēku ietekmējošam
-                    faktoram piešķirta skaitliska vienība (svars), kas raksturo tā ietekmi uz meža
+                    <b>RISK</b> - mežu ugunsgrēku kopējā riska vērtība. Katram ugunsgrēku ietekmējošam 
+                    faktoram piešķirta skaitliska vienība (svars), kas raksturo tā ietekmi uz meža 
                     ugunsgrēku izcelšanos vai izplatīšanos.<br/>
                     <br/>
                     Katram meža punktam uz kartes ir aprēķināts ugunsgrēka izplatības vai izcelšanas risks pēc šādam formulām:<br/>
@@ -45,19 +45,19 @@ const Faq = () => {
                     Kas ir JAC?
                 </h1>
                 <p>
-                    Žakāra indeksa (JAC) metode ir jauna metode, kas potenciāli var tikt
-                    izmantota meža ugunsgrēku ietekmējošo faktoru svara vērtību noteikšanai un pārbaudei. Žakāra
-                    indekss, ko dēvē arī par Žakāra līdzības koeficientu, ir statistisks rādītājs, ko izmanto, lai
+                    Žakāra indeksa (JAC) metode ir jauna metode, kas potenciāli var tikt 
+                    izmantota meža ugunsgrēku ietekmējošo faktoru svara vērtību noteikšanai un pārbaudei. Žakāra 
+                    indekss, ko dēvē arī par Žakāra līdzības koeficientu, ir statistisks rādītājs, ko izmanto, lai 
                     izprastu līdzības starp izlašu kopām.<br/>
-                    Šajā metodē izmantots princips, kurā visi ugunsgrēku ietekmējošie faktori tika
+                    Šajā metodē izmantots princips, kurā visi ugunsgrēku ietekmējošie faktori tika 
                     novērtēti kā vienlīdzīgi – izcelšanās faktoriem sākotnējā svara vērtība ir 8 
                     (deviņi faktori), izplatīšanās faktoriem sākotnējā svara vērtība 6 (divpadsmit 
                     faktori), katrai no riska situācijām veidojot pusi no kopējā ugunsgrēku riska. 
                     Lai iegūtu faktora svara vērtību, konkrētā faktora sākotnējā svara vērtība tiek 
-                    paaugstināta par 1 vienību, rezultātā iegūstot Žakāra indeksa izmaiņas augsta riska
-                    klases vērtībā ugunsgrēku vēsturiskajās teritorijās. Tas veicams katram faktoram
+                    paaugstināta par 1 vienību, rezultātā iegūstot Žakāra indeksa izmaiņas augsta riska 
+                    klases vērtībā ugunsgrēku vēsturiskajās teritorijās. Tas veicams katram faktoram 
                     individuāli. Iegūtā Žakāra indeksa izmaiņas vērtība tiek piešķirta kā faktora 
-                    svara vērtība. 
+                    svara vērtība.
                 </p>
             </div>
             <div>
@@ -65,13 +65,13 @@ const Faq = () => {
                     Kas ir AHP?
                 </h1>
                 <p>
-                    Analītiskās hierarhijas procesa (AHP) metode ir viena no visbiežāk izmantotajām
-                    metodēm pasaulē meža ugunsbīstamības ietekmējošo faktoru svara vērtību noteikšanā kā
-                    lēmumu pieņemšanas atbalsta sistēma. AHP metodes noteiktās svara vērtības balstītas uz
-                    ekspertu subjektīvu faktoru svara vērtību novērtējumu, kas transformēts kvantitatīvos lielumos,
+                    Analītiskās hierarhijas procesa (AHP) metode ir viena no visbiežāk izmantotajām 
+                    metodēm pasaulē meža ugunsbīstamības ietekmējošo faktoru svara vērtību noteikšanā kā 
+                    lēmumu pieņemšanas atbalsta sistēma. AHP metodes noteiktās svara vērtības balstītas uz 
+                    ekspertu subjektīvu faktoru svara vērtību novērtējumu, kas transformēts kvantitatīvos lielumos, 
                     attēlojot objektīvo realitāti.<br/>
-                    Faktori darbojas kā kritēriji, starp kuriem nosaka prioritāro
-                    faktoru un reižu skaitu, par cik tas ir nozīmīgāks meža ugunsgrēku riska ietekmēšanā nekā
+                    Faktori darbojas kā kritēriji, starp kuriem nosaka prioritāro 
+                    faktoru un reižu skaitu, par cik tas ir nozīmīgāks meža ugunsgrēku riska ietekmēšanā nekā 
                     salīdzinošais faktors.
                 </p>
             </div>
@@ -80,9 +80,9 @@ const Faq = () => {
                     Kas ir MLC?
                 </h1>
                 <p>
-                    Maximum likelihood classification (MLC) algoritma metode ir viena no populārākajām
-                    klasifikācijas metodēm Zemes tālizpētē. Metodes līdzšinējais pielietojums mežsaimniecībā ir
-                    bijis ugunsgrēku identificēšana satelītattēlos. Šajā pētījumā mākslīgā intelekta prognozēšanas
+                    Maximum likelihood classification (MLC) algoritma metode ir viena no populārākajām 
+                    klasifikācijas metodēm Zemes tālizpētē. Metodes līdzšinējais pielietojums mežsaimniecībā ir 
+                    bijis ugunsgrēku identificēšana satelītattēlos. Šajā pētījumā mākslīgā intelekta prognozēšanas 
                     rīks izmantots faktoru svara vērtību noteikšanā.
                 </p>
             </div>
@@ -91,10 +91,22 @@ const Faq = () => {
                     Teorētisks modelēšanas rīks, vai close-to-real time meža ugunsgrēka bīstabības indikātors?
                 </h1>
                 <p>
-                    Lai gan šķiet, ka mūsu rīks ir stipri teorētisks modelis, tomēr tā nav.
-                    Laikapstākļu dati tiek ņemti reāllaikā, kas nozīmē, ka RISK atšķirsies atkarībā
+                    Lai gan šķiet, ka mūsu rīks ir stipri teorētisks modelis, tomēr tā nav. 
+                    Laikapstākļu dati tiek ņemti reāllaikā, kas nozīmē, ka RISK atšķirsies atkarībā 
                     no tiem. Atzīstam, ka neņemam vērā visus faktorus, kas ietekmē gan 
-                    ugunsgrēka izplatību, gan ugunsgrēka rašanos, tāpēc mūsu pieteikums nav tas precīzākais.
+                    ugunsgrēka izplatību, gan ugunsgrēka rašanos, tāpēc mūsu pieteikums nav tas precīzākais. 
+                </p>
+            </div>
+            <div>
+                <h1>
+                    Kāpēc kartē viss ir sarkans?
+                </h1>
+                <p>
+                Diemžēl, izmantojot dažas metodes, var redzēt, ka gandrīz visi punkti kartē ir iekrāsoti 
+                sarkanā krāsā. Tas galvenokārt ir saistīts ar faktu, ka darbā tiek izmantoti daļēji 
+                neprecīzi dati, īpaši par laikapstākļiem. Arī darbā tiek izmantotas ne visas datu kopas, 
+                kas ir aprakstītas bakalaura darbā. Nākotnē izstrādes komanda plāno uzlabot precizitāti, 
+                iegūstot trūkstošās datu kopas un iegūstot precīzākus laikapstākļu datus.
                 </p>
             </div>
         </div>
